8001c3177c43557d9aeee7469390f6b354eb6778
mangezi <ngeziman@live.com>
2025-11-30 22:04:51 -0800

    Update metadata in mxm-mission-reward-tracker
    Updated version and description for clarity.

--- mxm-mission-reward-tracker.meta.js
+++ mxm-mission-reward-tracker.meta.js
@@ -5,4 +5,6 @@
 // @description  v5.2.0 Day/Week Logic + Portfolio + FX + Sync
 // @match        https://curators.musixmatch.com/*
 // @match        https://curators-beta.musixmatch.com/*
+// @downloadURL   https://raw.githubusercontent.com/AshtonLG3/MxM-Mission-Reward-Tracker/main/MxM-Tracker.user.js
+// @updateURL     https://raw.githubusercontent.com/AshtonLG3/MxM-Mission-Reward-Tracker/main/MxM-Tracker.meta.js
 // ==/UserScript==
