aa88b493c074531c7fd6fc5f22adbd42aeed7953
mangezi <ngeziman@live.com>
2025-11-30 21:57:25 -0800

    Create metadata for MxM Mission Reward Tracker
    Add metadata for MxM Mission Reward Tracker script

---
+++ mxm-mission-reward-tracker.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         MxM Mission Reward Tracker (Final Merge v6.4.0)
+// @namespace    mxm-tools
+// @version      6.4.0
+// @description  v5.2.0 Day/Week Logic + Portfolio + FX + Sync
+// @match        https://curators.musixmatch.com/*
+// @match        https://curators-beta.musixmatch.com/*
+// ==/UserScript==
