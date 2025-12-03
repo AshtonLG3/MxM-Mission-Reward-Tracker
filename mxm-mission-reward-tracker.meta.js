ce9b483581ca0a983f646f875aab63d310c41d44
mangezi <ngeziman@live.com>
2025-12-03 07:20:26 -0800

    Update script metadata to version 6.9.5

--- mxm-mission-reward-tracker.meta.js
+++ mxm-mission-reward-tracker.meta.js
@@ -1,8 +1,9 @@
 // ==UserScript==
-// @name         MxM Mission Reward Tracker (v6.9.0 Stable)
+// @name         MxM Mission Reward Tracker (v6.9.5 Tuned)
 // @namespace    mxm-tools
-// @version      6.9.2
-// @description  Day/Week counters + Portfolio + Live FX + Cross-Tab Sync (No Notion)
+// @version      6.9.5
+// @description  Day/Week counters + Portfolio + Live Rates (ZAR/NGN/KES) + Anti-Freeze (1.3 Tolerance)
+// @author       Richard Mangezi Muketa
 // @match        https://curators.musixmatch.com/*
 // @match        https://curators-beta.musixmatch.com/*
 // @downloadURL   https://raw.githubusercontent.com/AshtonLG3/MxM-Mission-Reward-Tracker/main/MxM-Tracker.user.js
